6feac64e54a08d9d88887a160724162057e23f7b
Tanya Gladkova <tanya.gladkova.2013@mail.ru>
2022-03-22 15:08:52 +0300

    Initial commit

---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,18 @@
+public class AuthTest {
+    public static final CREDS =  """
+            {
+                "username" : "admin",
+                "password" : "password123"
+            }
+            """;
+
+    @Test
+    public void getTokenTest(){
+        given()
+                .body
+        when()
+                .post
+        then()
+                .statueCode
+    }
+}
